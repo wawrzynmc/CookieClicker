7cbfad55985c327e66aec28b7caf333a2a47b369
wawrzynmc <karol.wawrzenczyk@gmail.com>
2021-03-16 18:36:23 +0100

    Implement Logo component

---
+++ client/src/components/atoms/Logo/Logo.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Typography, useTheme } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
+import IconButton from '../IconButton/IconButton';
+
+function Logo() {
+    const theme = useTheme();
+
+    return (
+        <IconButton edge="start" disableHoverEffects>
+            <StyledTypography
+                component={NavLink}
+                to="/home"
+                variant="h1"
+                color="textPrimary"
+                theme={theme}
+            >
+                C<StyledSpan>🍪</StyledSpan>
+                <StyledSpan>🍪</StyledSpan>kie Clicker
+            </StyledTypography>
+        </IconButton>
+    );
+}
+
+// -- styled components
+const StyledTypography = styled(({ ...props }) => <Typography {...props} />)`
+    && {
+        text-decoration: none;
+
+        @media (max-width: 400px) {
+            font-size: 1.4rem;
+        }
+    }
+`;
+
+const StyledSpan = styled.span`
+    font-size: 0.6em;
+`;
+
+export default Logo;
