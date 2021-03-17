227f93e2c318de7e84a6469fad6d18b9da5cf769
wawrzynmc <karol.wawrzenczyk@gmail.com>
2021-03-17 15:50:59 +0100

    ✨ Implement utils functions
    calculateLevel
    load/saveToLocalStorage

---
+++ client/src/shared/utils.js
@@ -0,0 +1,29 @@
+// * -- cookies calculate level function
+export function calculateLevel(punctions, startingLevel = 10) {
+    if (punctions < startingLevel) {
+        return 1;
+    }
+
+    return 1 + calculateLevel(punctions, startingLevel * 2);
+}
+
+// * -- local storage functions
+export const loadFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('state');
+        if (serializedState === null) return undefined;
+        return JSON.parse(serializedState);
+    } catch (error) {
+        console.warn(error);
+        return undefined;
+    }
+};
+
+export const saveToLocalStorage = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('state', serializedState);
+    } catch (error) {
+        console.warn(error);
+    }
+};
