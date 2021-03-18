b2925e90ee647f59d944f333db701f0db4d1eae0
wawrzynmc <karol.wawrzenczyk@gmail.com>
2021-03-18 21:46:48 +0100

    💥 create models

---
+++ server/src/models/achivement.model.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+
+// * -- INTERFACES
+// ---- user attributes (required to create a user)
+interface AchivementAttrs {
+    title: string;
+    description: string;
+    points: number;
+    level: number;
+}
+
+// ---- user properties (User Document - properties of a single entity)
+interface AchivementDoc extends mongoose.Document {
+    title: string;
+    description: string;
+    points: number;
+    level: number;
+}
+
+// ---- user model properties and methods (represents entire collection of data)
+interface AchivementModel extends mongoose.Model<AchivementDoc> {
+    build(attrs: AchivementAttrs): AchivementDoc;
+}
+
+// * -- SCHEMA
+const achivementSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        points: {
+            type: Number,
+            min: 1
+        },
+        level: {
+            type: Number,
+            min: 1
+        },
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+            },
+            versionKey: false, // remove __v from reply
+        },
+    }
+);
+
+achivementSchema.statics.build = (attrs: AchivementAttrs) => {
+    return new Achivement(attrs);
+};
+
+const Achivement = mongoose.model<AchivementDoc, AchivementModel>('Achivement', achivementSchema);
+
+export { Achivement };
---
+++ server/src/models/user.model.ts
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import { Password } from '../utils/password';
+
+// * -- INTERFACES
+// ---- user attributes (required to create a user)
+interface UserAttrs {
+    email: string;
+    password: string;
+}
+
+// ---- user properties (User Document - properties of a single entity)
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+// ---- user model properties and methods (represents entire collection of data)
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// * -- SCHEMA
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        toJSON: {
+            transform(doc, ret) {
+                ret.id = ret._id;
+                delete ret._id;
+                delete ret.password;
+            },
+            versionKey: false, // remove __v from reply
+        },
+    }
+);
+
+userSchema.pre('save', async function (done) {
+    // -- run this only when password was modified during save
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
