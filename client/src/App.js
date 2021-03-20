8adfea7a7dbc8c0f65575fb6ae57fa5b59049b0c
wawrzynmc <karol.wawrzenczyk@gmail.com>
2021-03-20 18:25:14 +0100

    ⚡️ Add lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,45 @@
 // -- imports
-import React from 'react';
+import React, { Suspense } from 'react';
 import { CssBaseline, responsiveFontSizes, ThemeProvider } from '@material-ui/core';
 import { Route, Redirect, Switch, useLocation } from 'react-router-dom';
 
 // -- global styles
 import { GlobalStyles, theme } from './assets/styles/GlobalStyles';
+import Loader from './components/molecules/Loader/Loader';
 
 // -- internal components
 import Header from './components/organisms/Header/Header';
-import Achievements from './components/pages/Achievements';
-import Home from './components/pages/Home';
-import Shop from './components/pages/Shop';
-
+const Achievements = React.lazy(() => import('./components/pages/Achievements'));
+const Home = React.lazy(() => import('./components/pages/Home'));
+const Shop = React.lazy(() => import('./components/pages/Shop'));
 
 // * -- COMPONENT
 function App() {
     const location = useLocation();
 
     const routes = (
-            <Switch location={location} key={location.pathname}>
-                <Route path="/home" exact>
-                    <Home />
-                </Route>
-                <Route path="/achivements" exact>
-                    <Achievements />
-                </Route>
-                <Route path="/shop" exact>
-                    <Shop />
-                </Route>
-                <Redirect to="/home" />
-            </Switch>
+        <Switch location={location} key={location.pathname}>
+            <Route path="/home" exact>
+                <Home />
+            </Route>
+            <Route path="/achivements" exact>
+                <Achievements />
+            </Route>
+            <Route path="/shop" exact>
+                <Shop />
+            </Route>
+            <Redirect to="/home" />
+        </Switch>
     );
 
     return (
         <ThemeProvider theme={responsiveFontSizes(theme)}>
             <Header />
             <main>
-                <GlobalStyles />
-                {routes}
+                <Suspense fallback={<Loader />}>
+                    <GlobalStyles />
+                    {routes}
+                </Suspense>
             </main>
             <CssBaseline />
         </ThemeProvider>
