958b44a77937cc682e8d8cc664bfb8fb66954e31
wawrzynmc <karol.wawrzenczyk@gmail.com>
2021-03-22 21:28:18 +0100

    💄 Implement WiP for shop page

--- client/src/components/pages/Shop.js
+++ client/src/components/pages/Shop.js
@@ -1,17 +1,22 @@
 // -- imports
 import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
 import { Container } from '@material-ui/core';
-
+import wipSVG from '../../assets/images/wip.svg';
 
 // * -- COMPONENT
 function Shop() {
     return (
-        <Container style={{ height: '100%' }}>
-            SHOP
+        <Container style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <StyledImage whileHover={{ scale: 1.1, cursor: 'pointer' }} src={wipSVG} alt="wip" />
         </Container>
     );
 }
 
 // -- styled components
+const StyledImage = styled(motion.img)`
+    width: 50%;
+`;
 
 export default Shop;
